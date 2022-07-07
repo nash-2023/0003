5031e756b5e391c309eee8704529fe64b86e67bd
nash-2023 <abdalla.fat7y@hotmail.com>
2022-07-07 15:28:45 +0200

    mehwar pc

--- app_3/lib/tDrobDnBtn.dart
+++ app_3/lib/tDrobDnBtn.dart
@@ -8,7 +8,7 @@ class Tdrbdnbtn extends StatefulWidget {
 }
 
 class _TdrbdnbtnState extends State<Tdrbdnbtn> {
-  var _selectedCountry;
+  var _selectedCountry; //= "Egp";
   @override
   Widget build(BuildContext context) {
     var _items = ['Arc', 'Doc', 'Egp', 'Uae', 'Xyz', 'Dwg', 'Gid'];
@@ -30,20 +30,23 @@ class _TdrbdnbtnState extends State<Tdrbdnbtn> {
           // alignment: Alignment.topCenter,
           width: 300.0,
           height: 70.0,
-          // padding: EdgeInsets.all(0.0),
+          // padding: EdgeInsets.only(left: 10.0),
           margin: EdgeInsets.only(left: 100.0, top: 100.0),
+          // alignment: Alignment.center,
           decoration: BoxDecoration(
             // color: Color.fromARGB(255, 143, 92, 238),
             border: Border.all(
               color: Colors.black38,
               width: 2.0,
             ),
-            borderRadius: BorderRadius.circular(5.0),
+            borderRadius: BorderRadius.circular(10.0),
           ),
           child: DropdownButton(
             items: _drbDnMnuItms,
             value: _selectedCountry,
-            hint: Text("Select Your country"),
+            hint: Text(
+              "    Select Your country",
+            ),
             onChanged: _chngCntry,
             underline: Divider(
               color: null,
@@ -55,14 +58,15 @@ class _TdrbdnbtnState extends State<Tdrbdnbtn> {
               // color: Colors.red,
               fontSize: 16.0,
               fontWeight: FontWeight.w900,
-              fontFamily: "Calibri",
+              // decoration: TextDecoration.overline,
+              // decorationStyle: TextDecorationStyle.wavy,
             ),
-            icon: Icon(
-              Icons.add_circle,
-              // color: Colors.green,   // if this hava a value, it will conquer
-              // size: 30.0,            // if this hava a value, it will conquer
-            ),
-            isExpanded: true,
+            // // icon: Icon(
+            //   Icons.add_circle,
+            //   // color: Colors.green,   // if this hava a value, it will conquer
+            //   // size: 30.0,            // if this hava a value, it will conquer
+            // ),
+            // // isExpanded: true,
             // isDense: true,
             // alignment: Alignment.center,
             // borderRadius: BorderRadius.circular(5.0),
@@ -70,8 +74,9 @@ class _TdrbdnbtnState extends State<Tdrbdnbtn> {
             itemHeight: 70.0,
             // focusColor: Color.fromARGB(255, 164, 243, 127),
             // dropdownColor: Color.fromARGB(255, 109, 243, 165),
-            iconSize: 30.0,
+            iconSize: 20.0,
             iconEnabledColor: Colors.green,
+            isExpanded: true,
           ),
 
           //*********
@@ -91,16 +96,10 @@ class _TdrbdnbtnState extends State<Tdrbdnbtn> {
           //   borderRadius: BorderRadius.circular(5.0),
           // ),
           child: ListTile(
-            leading: Icon(
-              Icons.ac_unit,
-              size: 20,
-            ), //Text("leading"),
+            leading: Icon(Icons.ac_unit, size: 20), //Text("leading"),
             title: Text("Air Conditioning Unit"),
             subtitle: Text("Toggle"),
-            trailing: Icon(
-              Icons.expand_more,
-              size: 20,
-            ),
+            trailing: Icon(Icons.expand_more, size: 20),
             isThreeLine: true,
             dense: false,
             shape: Border.all(
